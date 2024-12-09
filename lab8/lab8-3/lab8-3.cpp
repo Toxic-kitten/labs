030381052e66ac9fd676fc6b362a5d5854db36a3
Toxic-kitten <kostyaxantonow09@gmail.com>
2024-12-09 17:24:11 +0200

    Сделал лабу №9 и начал работу над №8-3

--- lab8/lab8-3/lab8-3.cpp
+++ lab8/lab8-3/lab8-3.cpp
@@ -2,7 +2,6 @@
 #include <fstream>
 #include <cctype>
 #include <string>
-#include <array>
 using namespace std;
 
 const int N_max = 5120;
@@ -29,9 +28,51 @@ void write(int& n, string words[N_max]){
     }
 }
 
+void search_popular_letters(int n, int count_words[256], string words[N_max]){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<words[i].length();j++){
+            char c = tolower(words[i][j]);
+            count_words[c]++;
+        }
+    }
+}
+
+void add_code_words(int n, int code_words[256]){
+    int k = 0;
+    for(int i=0;i<256;i++){
+        code_words[i] = k;
+        k+=1;
+    }
+}
+
+void sort_popular_letters(int n, int count_words[256], int code_words[256]){
+    for(int i=0;i<256-1;i++){
+        for(int j=i+1;j<256;j++){
+            if(count_words[i] < count_words[j]){
+                swap(count_words[i], count_words[j]);
+                swap(code_words[i], code_words[j]);
+            }
+        }
+    }
+}
+
 int main(){
     int n;
     string words[N_max];
+    int code_words[256]{};
+    int count_words[256]{};
     read(n, words);
-    write(n, words);
+    add_code_words(n, code_words);
+    search_popular_letters(n, count_words, words);
+    sort_popular_letters(n, count_words, code_words);
+    for(int i=0;i<256;i++){
+        char bykva = code_words[i];
+        if (count_words[i] != 0){
+            cout << count_words[i] << ' ';
+            cout << code_words[i] << ' ';
+            cout << bykva << '\n';
+        }
+    }
+
+    // write(n, words);
 }
--- lab9/lab9.cpp
+++ lab9/lab9.cpp
@@ -7,10 +7,16 @@ int main(){
     int n = 1000;
     // создаем указатель p на адрес переменной n
     int *p = &n;
-    // приводим указатель к типу char равному 1 байту, для получения каждого байта переменной n
-    char *pchar {(char *)(p)};
-    int x = ((unsigned char *)(p))[0];
+    // приводим указатель к типу unsigned char равному 1 байту, для получения каждого байта переменной n
     // разименновываем указатель чтобы получить первый и последующие байты числа n
-    cout << x << "\n";
-    // cout << "First byte: " << *pchar << "\n";
-}
+    // char *pchar {(char *)(p)};
+    int x = ((unsigned char *)(p))[0];
+    int y = ((unsigned char *)(p))[1];
+    int z = ((unsigned char *)(p))[2];
+    int t = ((unsigned char *)(p))[3];
+    
+    cout << "First byte: " << x << "\n";
+    cout << "Second byte: " << y << "\n";
+    cout << "Third byte: " << z << "\n";
+    cout << "Fourth byte: " << t << "\n";
+} 
