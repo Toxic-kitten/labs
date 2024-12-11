1bf86434796bb12a575947b4fcaa6254decd7b4a
Toxic-kitten <kostyaxantonow09@gmail.com>
2024-12-11 15:47:19 +0200

    Сделал лабу №9

--- lab8/lab8-3/lab8-3.cpp
+++ lab8/lab8-3/lab8-3.cpp
@@ -56,23 +56,61 @@ void sort_popular_letters(int n, int count_words[256], int code_words[256]){
     }
 }
 
+void top_five_letters(int count_words[256], int code_words[256], int five_words_count[5], int five_words[5]){
+    for(int i=0;i<5;i++){
+        int bykva = code_words[i];
+        five_words_count[i] = count_words[i];
+        five_words[i] = bykva;
+    }
+}
+
+void found_letters(int n, string words[N_max], int five_words[5]){
+    for(int i=0;i<n;i++){
+        int cnt = 0;
+        string pop_lett_in_word[5];
+        for(int j=0;j<words[i].length();j++){
+            for(int k=0;k<5;k++){
+                char bykva = tolower(words[i][j]);
+                char pop_bykva = five_words[k];
+                if(bykva == pop_bykva){
+                    cnt += 1;
+                    pop_lett_in_word[k] = pop_bykva;
+                }
+                // if (cnt >= 4){
+                //     toUpper(n, i, words);
+                //     // words[i] += "(";
+                //     // for(int r=0;r<5;r++){
+                //     //     words[i] += pop_lett_in_word[r];
+                //     // }
+                //     // words[i] += ")";
+                // }
+            }
+        }
+    }
+}
+
+void toUpper(int n, int index, string words[N_max]){
+    int i = index;
+    for(int j=0;j<words[i].length();j++){
+        words[i][j] = toupper(words[i][j]);
+    }
+}
+
 int main(){
     int n;
     string words[N_max];
     int code_words[256]{};
     int count_words[256]{};
+    int five_words[5]{};
+    int five_words_count[5]{};
     read(n, words);
     add_code_words(n, code_words);
     search_popular_letters(n, count_words, words);
     sort_popular_letters(n, count_words, code_words);
-    for(int i=0;i<256;i++){
-        char bykva = code_words[i];
-        if (count_words[i] != 0){
-            cout << count_words[i] << ' ';
-            cout << code_words[i] << ' ';
-            cout << bykva << '\n';
-        }
-    }
-
-    // write(n, words);
+    top_five_letters(count_words, code_words, five_words_count, five_words);
+    found_letters(n, words, five_words);
+    // for(int i=0;i<5;i++){
+    //     toUpper(n, i, words);
+    // }
+    write(n, words);
 }
--- lab9/lab9.cpp
+++ lab9/lab9.cpp
@@ -6,17 +6,23 @@ int main(){
     // создаем переменную n типа int
     int n = 1000;
     // создаем указатель p на адрес переменной n
-    int *p = &n;
+    int* p = &n;
     // приводим указатель к типу unsigned char равному 1 байту, для получения каждого байта переменной n
-    // разименновываем указатель чтобы получить первый и последующие байты числа n
-    // char *pchar {(char *)(p)};
-    int x = ((unsigned char *)(p))[0];
-    int y = ((unsigned char *)(p))[1];
-    int z = ((unsigned char *)(p))[2];
-    int t = ((unsigned char *)(p))[3];
+    unsigned char* p_char = (unsigned char *)(p);
+    // разименновываем указатель и приводим к типу int чтобы получить первый и последующие байты числа n
+    // выводим каждый байт числа с помощью прибавления единицы к указателю до разименнации
+    for(int i=0;i<4;i++){
+        cout << "Byte " << i << ": " << (unsigned int)(*(p_char+i)) << "\n";
+    }
+
+    // Вариант 2
+    // int x = ((unsigned char *)(p))[0]; // *(((unsigned char *)(p)) + 1)
+    // int y = ((unsigned char *)(p))[1];
+    // int z = ((unsigned char *)(p))[2];
+    // int t = ((unsigned char *)(p))[3];
     
-    cout << "First byte: " << x << "\n";
-    cout << "Second byte: " << y << "\n";
-    cout << "Third byte: " << z << "\n";
-    cout << "Fourth byte: " << t << "\n";
+    // cout << "First byte: " << x << "\n";
+    // cout << "Second byte: " << y << "\n";
+    // cout << "Third byte: " << z << "\n";
+    // cout << "Fourth byte: " << t << "\n";
 } 
